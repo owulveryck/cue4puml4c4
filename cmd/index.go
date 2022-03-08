63a5ad174525fd0e42ddc3596535fb74558f5479
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2022-03-08 14:51:17 +0100

    feat: navigate into svg

--- cmd/index.go
+++ cmd/index.go
@@ -42,24 +42,12 @@ const (
 				clear: both;
 			}
 	
-			object {
-	
-				max-width: 100%;
-				max-height: 100%;
-				bottom: 0;
-				left: 0;
-				margin: auto;
-				overflow: auto;
+			.container {
+				width: 91vw;
+				height: 100%;
+				left: 16vh;
 				position: fixed;
-				right: 0;
-				top: 0;
-				-o-object-fit: contain;
-				object-fit: contain;
-	
-				aspect-ratio: inherit;
-				max-height: 100%;
-				max-width: 100%;
-	
+				float: none;
 			}
 		</style>
 		<title>{{.Name}}</title>
@@ -67,27 +55,61 @@ const (
 	
 	<body>
 		<div class="row">
-			<div class="column left" style="background-color:#aaa;">
-				<button class="btn"><i class="fa fa-download"><a id="download" download="{{.Name}}.svg"></i> Download SVG</a></button>
+			<div id="colLeft" class="column left" style="background-color:#aaa;">
+				<button class="btn"><i class="fa fa-download"></i> Download SVG</button>
 				<button class="btn" onclick="togglePUML()">Display PUML Content</button>
-
 				<ul>
-					<li><a href="{{.URL.Path}}/../../">..<a></li>
+					<li><a href="../">..</a></li>
 					{{range .DirEntries}}
 					<li><a href="{{.Path}}">{{.Name}}</a></li>
 					{{end}}
 				</ul>
+				Zoom: <span id="zoomValue">1</span>
 			</div>
-			<div class="column right" style="background-color:#bbb;">
-				<pre id="cue">Generating picture...</pre>
-				<hr>
-				<pre id="plantuml"  style="visibility: hidden;"></pre>
-				<object id="output" type="image/svg+xml"></object>
+			<div id="colRight" class="column right" style="background-color:#bbb;">
+				<div class="container" id="svgContainer">
+				 <svg/>
+				</div>
+					<pre id="cue">Generating picture...</pre>
+				<pre id="plantuml" style="visibility: hidden;"></pre>
 			</div>
+		</div>
 		</div>
 	</body>
 	
 	<script type="text/javascript">
+	
+		const svgContainer = document.getElementById("svgContainer");
+		const right = document.getElementById("colRight").getBoundingClientRect();
+		const left = document.getElementById("colLeft").getBoundingClientRect();
+		svgImage = document.getElementsByTagName("svg")[0];
+		svgImage.style = ""
+		svgImage.removeAttribute("width");
+		svgImage.removeAttribute("height");
+		console.log(window.innerHeight)
+	
+	
+		// Full height, including the scroll part
+		const fullHeight = Math.max(
+			document.body.scrollHeight,
+			document.documentElement.scrollHeight,
+			document.body.offsetHeight,
+			document.documentElement.offsetHeight,
+			document.body.clientHeight,
+			document.documentElement.clientHeight
+		);
+		svgImage.setAttribute('viewBox', ` + "`" + `0 0 ${right.width} ${fullHeight}` + "`" + `);
+		console.log(fullHeight)
+	
+		var viewBox = { x: 0, y: 0, w: right.width, h: fullHeight };
+		//svgImage.setAttribute('viewBox', ` + "`" + `${viewBox.x} ${viewBox.y} ${viewBox.w} ${viewBox.h}` + "`" + `);
+		const svgSize = { w: svgImage.clientWidth, h: svgImage.clientHeight };
+		var isPanning = false;
+		var startPoint = { x: 0, y: 0 };
+		var endPoint = { x: 0, y: 0 };;
+		var scale = 1;
+	
+	
 		var url = "ws{{if eq .URL.Scheme "https"}}s{{end}}://{{.URL.Host}}{{.URL.Path}}";
 		ws = new WebSocket(url);
 	
@@ -102,10 +124,15 @@ const (
 		ws.onmessage = function (e) {
 			console.log("[onmessage] receive message.");
 			var res = JSON.parse(e.data);
-			document.getElementById("output").setAttribute("data", "data:image/svg+xml;utf8;base64," + res["image"]); // decodeURIComponent(escape(window.atob(res["image"]))))
-			document.getElementById("download").setAttribute("href","data:image/svg+xml;utf8;base64," + res["image"]) ;
+			document.getElementById("svgContainer").innerHTML = decodeURIComponent(escape(window.atob(res["image"])))// setAttribute("data", "data:image/svg+xml;utf8;base64," + res["image"]); // decodeURIComponent(escape(window.atob(res["image"]))))
 			document.getElementById("cue").innerHTML = decodeURIComponent(escape(window.atob(res["cue"])))
 			document.getElementById("plantuml").innerHTML = decodeURIComponent(escape(window.atob(res["plantuml"])))
+			svgImage = document.getElementsByTagName("svg")[0];
+			svgImage.style = ""
+			svgImage.removeAttribute("width");
+			svgImage.removeAttribute("height");
+			svgImage.setAttribute('viewBox', ` + "`" + `0 0 ${right.width} ${fullHeight}` + "`" + `);
+
 		}
 	
 		ws.onclose = function (e) {
@@ -114,6 +141,60 @@ const (
 	
 		ws.onerror = function (e) {
 			console.log("[onerror] error!");
+		}
+		function togglePUML() {
+			if (document.getElementById("plantuml").style.visibility == "hidden") {
+				document.getElementById("plantuml").style.visibility = "visible";
+			} else {
+				document.getElementById("plantuml").style.visibility = "hidden";
+			}
+		}
+	
+		svgContainer.onmousewheel = function (e) {
+			e.preventDefault();
+			var w = viewBox.w;
+			var h = viewBox.h;
+			var mx = e.offsetX;//mouse x  
+			var my = e.offsetY;
+			var dw = w * Math.sign(e.deltaY) * 0.05;
+			var dh = h * Math.sign(e.deltaY) * 0.05;
+			var dx = dw * mx / svgSize.w;
+			var dy = dh * my / svgSize.h;
+			viewBox = { x: viewBox.x + dx, y: viewBox.y + dy, w: viewBox.w - dw, h: viewBox.h - dh };
+			scale = svgSize.w / viewBox.w;
+			zoomValue.innerText = ` + "`" + `${Math.round(scale * 100) / 100}` + "`" + `;
+			svgImage.setAttribute('viewBox', ` + "`" + `${viewBox.x} ${viewBox.y} ${viewBox.w} ${viewBox.h}` + "`" + `);
+		}
+	
+	
+		svgContainer.onmousedown = function (e) {
+			isPanning = true;
+			startPoint = { x: e.x, y: e.y };
+		}
+	
+		svgContainer.onmousemove = function (e) {
+			if (isPanning) {
+				endPoint = { x: e.x, y: e.y };
+				var dx = (startPoint.x - endPoint.x) / scale;
+				var dy = (startPoint.y - endPoint.y) / scale;
+				var movedViewBox = { x: viewBox.x + dx, y: viewBox.y + dy, w: viewBox.w, h: viewBox.h };
+				svgImage.setAttribute('viewBox', ` + "`" + `${movedViewBox.x} ${movedViewBox.y} ${movedViewBox.w} ${movedViewBox.h}` + "`" + `);
+			}
+		}
+	
+		svgContainer.onmouseup = function (e) {
+			if (isPanning) {
+				endPoint = { x: e.x, y: e.y };
+				var dx = (startPoint.x - endPoint.x) / scale;
+				var dy = (startPoint.y - endPoint.y) / scale;
+				viewBox = { x: viewBox.x + dx, y: viewBox.y + dy, w: viewBox.w, h: viewBox.h };
+				svgImage.setAttribute('viewBox', ` + "`" + `${viewBox.x} ${viewBox.y} ${viewBox.w} ${viewBox.h}` + "`" + `);
+				isPanning = false;
+			}
+		}
+	
+		svgContainer.onmouseleave = function (e) {
+			isPanning = false;
 		}
 		function togglePUML() {
 			if (document.getElementById("plantuml").style.visibility == "hidden") {
